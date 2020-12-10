f9fd291a007c9ee7933673f0b32a31758f60175c
Ayoung Sim <a.sim@samsung.com>
2020-12-10 15:13:10 +0900

    Revert "media: mfc: Fix the MFC reset sequence"
    This reverts commit 42f01611680475a7b6cfe18190eed551c5218080.
    
    Change-Id: I340197dc9222939eb20397c4a40fa631593d5f15
    (cherry picked from commit 0647388aaced2fcd9c5a8d17f858e97f14ef48ca)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/mfc/mfc_core_meerkat.c
+++ drivers/media/platform/exynos/mfc/mfc_core_meerkat.c
@@ -717,9 +717,9 @@ static void __mfc_dump_info(struct mfc_core *core)
 		return;
 	}
 
+	__mfc_save_logging_sfr(core);
 	__mfc_dump_buffer_info(core);
 	__mfc_dump_regs(core);
-	__mfc_save_logging_sfr(core);
 }
 
 static int __mfc_store_dump_state(struct mfc_dev *dev, char *buf)
