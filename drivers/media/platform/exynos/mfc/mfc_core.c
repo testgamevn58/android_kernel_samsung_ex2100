9a536703916cb89c9b92a34c1362512640e1a34c
Sunyoung Kang <sy0816.kang@samsung.com>
2021-04-21 15:00:52 +0900

    media: mfc: fix the wrong sysmmu AxID info
    When sysmmu page fault occurs, the AxID info was wrong so this fix it.
    
    Bug: 184730454
    
    Change-Id: Ifc8cdf89f8e0f8568edcee23a22c766e9b8265b6
    Signed-off-by: Sunyoung Kang <sy0816.kang@samsung.com>
    (cherry picked from commit 75f1b04c3e6521f11d3b92b1b7c0cb6d73594578)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/mfc/mfc_core.c
+++ drivers/media/platform/exynos/mfc/mfc_core.c
@@ -175,9 +175,9 @@ int mfc_core_sysmmu_fault_handler(struct iommu_fault *fault, void *param)
 	core->logging_data->fault_addr = (unsigned int)(fault->event.addr);
 
 	mfc_core_err("MFC-%d SysMMU PAGE FAULT at %#lx (AxID: %#x)\n",
-			core->id, (unsigned int)(fault->event.addr), trans_info);
+			core->id, (unsigned int)(fault->event.addr), core->logging_data->fault_trans_info);
 	MFC_TRACE_CORE("MFC-%d SysMMU PAGE FAULT at %#lx (AxID: %#x)\n",
-			core->id, (unsigned int)(fault->event.addr), trans_info);
+			core->id, (unsigned int)(fault->event.addr), core->logging_data->fault_trans_info);
 
 	call_dop(core, dump_and_stop_debug_mode, core);
 
