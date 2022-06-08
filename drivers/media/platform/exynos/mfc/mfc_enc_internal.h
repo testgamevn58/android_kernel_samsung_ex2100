16e929fea37b225e4bc9702ec776c59131c7a3d1
AyoungSim <a.sim@samsung.com>
2022-06-08 19:39:44 +0900

    media: mfc: change the format according to various conditions
    The format should be changed according to various conditions.
    1. compress (SBWC or not)
    2. bit depth (8bit or 10bit)
    3. chroma order (CbCr or CrCb)
    4. SoC supported 10bit type (P010/P210 or 8+2)
    5. component in memory (multi or single)
    
    Bug: 229547170
    Bug: 238360304
    Test: Build PASS
    Signed-off-by: AyoungSim <a.sim@samsung.com>
    Change-Id: I973131fd43c750c28798ca73af3d3806e69874aa
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/mfc/mfc_enc_internal.h
+++ drivers/media/platform/exynos/mfc/mfc_enc_internal.h
@@ -118,7 +118,7 @@ static struct mfc_fmt enc_formats[] = {
 		.mem_planes = 2,
 	},
 	{
-		.name = "4:2:0 2 Planes Y/CbCr 8bit Compress (SBWC)",
+		.name = "4:2:0 2 Planes CbCr 8bit Compress (SBWC)",
 		.fourcc = V4L2_PIX_FMT_NV12M_SBWC_8B,
 		.codec_mode = MFC_FORMATS_NO_CODEC,
 		.type = MFC_FMT_FRAME | MFC_FMT_SBWC,
@@ -126,7 +126,7 @@ static struct mfc_fmt enc_formats[] = {
 		.mem_planes = 2,
 	},
 	{
-		.name = "4:2:0 2 Planes Y/CbCr 10bit Compress (SBWC)",
+		.name = "4:2:0 2 Planes CbCr 10bit Compress (SBWC)",
 		.fourcc = V4L2_PIX_FMT_NV12M_SBWC_10B,
 		.codec_mode = MFC_FORMATS_NO_CODEC,
 		.type = MFC_FMT_FRAME | MFC_FMT_SBWC,
@@ -134,7 +134,7 @@ static struct mfc_fmt enc_formats[] = {
 		.mem_planes = 2,
 	},
 	{
-		.name = "4:2:0 2 Planes Y/CrCb 8bit Compress (SBWC)",
+		.name = "4:2:0 2 Planes CrCb 8bit Compress (SBWC)",
 		.fourcc = V4L2_PIX_FMT_NV21M_SBWC_8B,
 		.codec_mode = MFC_FORMATS_NO_CODEC,
 		.type = MFC_FMT_FRAME | MFC_FMT_SBWC,
@@ -142,7 +142,7 @@ static struct mfc_fmt enc_formats[] = {
 		.mem_planes = 2,
 	},
 	{
-		.name = "4:2:0 2 Planes Y/CrCb 10bit Compress (SBWC)",
+		.name = "4:2:0 2 Planes CrCb 10bit Compress (SBWC)",
 		.fourcc = V4L2_PIX_FMT_NV21M_SBWC_10B,
 		.codec_mode = MFC_FORMATS_NO_CODEC,
 		.type = MFC_FMT_FRAME | MFC_FMT_SBWC,
