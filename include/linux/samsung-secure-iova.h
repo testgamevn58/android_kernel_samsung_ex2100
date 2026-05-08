59bfb1488a2cbc5bf81e0010934214fd2fc7ebf6
oscyre1006627 <165604309+oscyre1006627@users.noreply.github.com>
2026-05-08 20:40:01 +0900

    samsung-secure-iova: Import changes from s5e9925
    [Flopster101: add static inline stubs when CONFIG_SAMSUNG_SECURE_IOVA is disabled]
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/samsung-secure-iova.h
+++ include/linux/samsung-secure-iova.h
@@ -1,12 +1,15 @@
-/* SPDX-License-Identifier: GPL-2.0
- *
+// SPDX-License-Identifier: GPL-2.0
+/*
  * Samsung IOVA Management header
  *
  * Copyright (c) 2021 Samsung Electronics Co., Ltd.
  * Author: <hyesoo.yu@samsung.com> for Samsung
  */
 
-#if IS_ENABLED(CONFIG_EXYNOS_CONTENT_PATH_PROTECTION)
+#ifndef __SAMSUNG_SECURE_IOVA_H
+#define __SAMSUNG_SECURE_IOVA_H
+
+#if IS_ENABLED(CONFIG_SAMSUNG_SECURE_IOVA)
 unsigned long secure_iova_alloc(unsigned long size, unsigned int align);
 void secure_iova_free(unsigned long addr, unsigned long size);
 #else
@@ -14,6 +17,7 @@ static inline unsigned long secure_iova_alloc(unsigned long size, unsigned int a
 {
 	return 0;
 }
-
 static inline void secure_iova_free(unsigned long addr, unsigned long size) { }
 #endif
+
+#endif /* __SAMSUNG_SECURE_IOVA_H */
